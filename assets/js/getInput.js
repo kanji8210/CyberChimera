56f1c24a7e4cdec303c01deedb22d6ca2edaa76f
FlAvril <92847885+FCORMERAIS@users.noreply.github.com>
2023-11-14 14:28:41 +0100

    ajout page creation de compte

--- assets/js/getInput.js
+++ assets/js/getInput.js
@@ -10,3 +10,18 @@ document.getElementById("login__form").addEventListener("submit", function(event
     console.log("Password: " + password);
  
  });
+
+
+document.getElementById("register__form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du formulaire (rechargement de la page)
+ 
+    // Récupérer les valeurs des champs
+    var pseudo = document.getElementById("register-pseudo").value;
+    var email = document.getElementById("register-email").value;
+    var password = document.getElementById("register-pass").value;
+ 
+    // Afficher les valeurs dans la console
+    console.log("Email: " + email);
+    console.log("Password: " + password);
+ 
+ });
